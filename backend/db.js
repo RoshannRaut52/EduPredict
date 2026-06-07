b21803598d6bf8b938205fcb34a4a7e5d2d7ff47
Roshan <roshanrautscience@gmail.com>
2026-06-07 12:16:06 +0530

    restarting on supabase1

--- backend/db.js
+++ backend/db.js
@@ -9,9 +9,9 @@ const pool = new Pool({
 
 module.exports = pool;
 
-const { Pool } = require("pg");
-
 // supabase chatgpr pool
+
+//const { Pool } = require("pg");
 // const pool = new Pool({
 //     connectionString: process.env.DATABASE_URL,
 //     ssl: {
